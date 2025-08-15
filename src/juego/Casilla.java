f1dadacb4c064427b846e48c44fd0783710c15d3
Jocelyn300698 <jocelyncc1998@gmail.com>
2025-08-14 23:21:52 -0600

    Modificación a la clase casilla

--- src/juego/Casilla.java
+++ src/juego/Casilla.java
@@ -12,10 +12,17 @@ public class Casilla {
     private int posFila;
     private int posColumna;
     private boolean mina;
+    private boolean revelada;
+    private boolean marcada;
+    private int minasCercanas;
 
     public Casilla(int posFila, int posColumna) {
         this.posFila = posFila;
         this.posColumna = posColumna;
+        this.mina = false;
+        this.revelada = false;
+        this.marcada = false;
+        this.minasCercanas = 0;
     }
     
     public int getPosFila() {
@@ -40,6 +47,30 @@ public class Casilla {
 
     public void setMina(boolean mina) {
         this.mina = mina;
+    }
+
+    public boolean isRevelada() {
+        return revelada;
+    }
+
+    public void setRevelada(boolean revelada) {
+        this.revelada = revelada;
+    }
+
+    public boolean isMarcada() {
+        return marcada;
+    }
+
+    public void setMarcada(boolean marcada) {
+        this.marcada = marcada;
+    }
+
+    public int getMinasCercanas() {
+        return minasCercanas;
+    }
+
+    public void setMinasCercanas(int minasCercanas) {
+        this.minasCercanas = minasCercanas;
     }
     
 }
